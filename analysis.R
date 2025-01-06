6c7d85f26e12395ee50ceec66e5b4d9b9268817c
Shira Zadok <shirazadok@mail.tau.ac.il>
2025-01-06 19:20:41 +0200

    Creating our project and a file called 'analysis' that creates a general data frame (and saves it) that containes the following variables: subject; age; gender; rt; depression; sleep_duration.

---
+++ analysis.R
@@ -0,0 +1,15 @@
+#Assignment 9 by Shira Zadok ID 318958311
+
+#### Creating data file ----
+#N is not set:
+subject = seq(1,N,1)
+age = runif(N, 18, 60)
+gender = factor(sample(c('female', 'male'), N, replace = T))
+rt = runif(N, 200, 6000) #mean time in msec
+depression = runif(N, 0, 100) #depression scores
+sleep_duration = runif(N, 2, 12) #mean time in hours
+
+df <- data.frame(subject, age, gender, rt, depression, sleep_duration)
+
+#save
+save(df, file = "./df.rdata")
